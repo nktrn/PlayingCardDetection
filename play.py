065dfbebcefe79ca0b1a1eb405ece7900f5e0fa9
nktrn <nikita.aparovich>
2022-04-30 21:51:09 +0300

    update weights

--- play.py
+++ play.py
@@ -15,24 +15,28 @@ def draw_corners(image, corners):
     return cv2.polylines(image, [pts], True, COLOR, THICKNESS)
 
 
-def video(capture, model: CardModel):
+def video(capture, model: CardModel, out):
     while True:
         ret, frame = capture.read()
-
         corners = model.predict(frame)
         frame = draw_corners(frame, corners)
+
+        #out.write(frame)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     capture.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     imageprocess = ImageProcess((108, 192))
     model = CardModel(shape=(192, 108, 3), imageprocess=imageprocess)
-    model.load_wights(r'./models/model_w1.hdf5')
+    model.load_wights(r'./models/model_w2.hdf5')
     capture = cv2.VideoCapture(URL)
-    video(capture, model)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (480, 640))
+    video(capture, model, out)
